facd9e6eb52069af975358f52ef136af64f1c945
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2023-06-04 01:00:52 +0530

    updated clustering server

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
 from sklearn.cluster import AgglomerativeClustering
-import pymongo
 
+from flask_cors import CORS , cross_origin
 app = Flask(__name__)
-
+CORS(app)
 # # MongoDB connection
 # mongo_client = pymongo.MongoClient('mongodb+srv://mumbaihacks2023:rupalimam@cluster0.pdai2zp.mongodb.net/?retryWrites=true&w=majority')
 # db = mongo_client['test']
@@ -15,9 +15,11 @@ def cluster():
     # Get the JSON data from the request
     # data = list(collection.find())
     data = request.get_json()
-    
-
-
+    result =[]
+    for arr in data:
+        result.append(arr[0])
+        result.append(arr[3])
+        result.append(arr[4])
     return jsonify(data)
 
 # Run the Flask server
