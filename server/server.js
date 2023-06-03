dbaf1e9582475e82194811977e8dde50d8a08a0c
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2023-06-03 14:56:01 +0530

    mongodb connected console

--- server/server.js
+++ server/server.js
@@ -12,12 +12,15 @@ app.use(bodyParser.json({limit:"30 mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30 mb", extended: true}));
 app.use(cors());
 
-// app.use('/login', loginRoutes);
+app.use('/login', loginRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewURLParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => {
+        console.log('Mongodb Connected');
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    })
     .catch((error) => console.log(error.message));
 
 
