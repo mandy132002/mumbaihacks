9e9dcd16ada91712e56b07775c6ac4f06f6ed529
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2023-06-04 00:23:01 +0530

    changed the government home page

--- src/Components/Government/GovernmentHome.jsx
+++ src/Components/Government/GovernmentHome.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import Navbar from '../Citizen/Navbar'
+import { useState,useEffect } from 'react';
+import axios from 'axios';
 
 export default function GovernmentHome() {
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  const fetchRequests = async () => {
+    try {
+      const response = await axios.get('http://localhost:5002/complaints/getComplaints');
+      const fetchedRequests = response.data;
+      setRequests(fetchedRequests);
+    } catch (error) {
+      console.error('Error fetching requests:', error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/cluster', requests);
+      const clusters = response.data;
+      console.log(clusters);
+      // Handle the returned clusters data as desired
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  };
   return (
-    <div>GovernmentHome</div>
+    <>
+      <Navbar/>
+      <button onClick={handleSubmit}>Update Status</button>
+    </>
+
   )
 }
