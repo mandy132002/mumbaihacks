eaf266811ed01c45e8c8bd619aff3b83fd4f7c9f
Kishan Kokal <kokalkishan.official@gmail.com>
2023-06-03 14:06:35 +0530

    Add login component

---
+++ src/Components/Citizen/login.jsx
@@ -0,0 +1,33 @@
+import "./Login.css";
+
+function Login() {
+  return (
+    <>
+      <div className="form-container">
+        <form className="login-form">
+          <div className="internal-form-container">
+            <div>
+              <label htmlFor="userId">User ID: </label>
+            </div>
+            <div>
+              <input type="text" id="userId" />
+            </div>
+          </div>
+          <div className="internal-form-container">
+            <div>
+              <label htmlFor="password">Password: </label>
+            </div>
+            <div>
+              <input type="password" id="password" />
+            </div>
+          </div>
+          <div className="internal-form-container">
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Login;
